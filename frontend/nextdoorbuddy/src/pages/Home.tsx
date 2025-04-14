b0882403155ee89d5f26d9266a68ca44c89ca08a
Lucas Verrecchia <lverrecchia@myges.fr>
2025-04-14 16:13:32 +0200

    init tailwind, stable 0.1.0

--- frontend/nextdoorbuddy/src/pages/Home.tsx
+++ frontend/nextdoorbuddy/src/pages/Home.tsx
@@ -1,15 +1,9 @@
 const Home = () => {
     return (
-        <div className="bg-green-100 p-4 text-center">
+        <div className="bg-green p-4 text-center">
             <h1 className="text-3xl font-bold text-green-800">
                 Bienvenue sur NextDoorBuddy !
             </h1>
-            <p className="mt-2 text-gray-600">
-                Connecte-toi ou inscris-toi pour rejoindre ton quartier.
-            </p>
-            <div className="bg-purple-300 text-red-600 text-5xl underline p-10">
-                Debug Tailwind Style, si le fond est violet ca fonctionne 🚀
-            </div>
         </div>
 
 
--- frontend/nextdoorbuddy/tailwind.config.js
+++ frontend/nextdoorbuddy/tailwind.config.js
@@ -5,7 +5,14 @@ export default {
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                green: '#10B981',  // Équivalent à green-500
+                red: '#EF4444',    // Équivalent à red-500
+                blue: '#3B82F6',   // Équivalent à blue-500
+                purple: '#8B5CF6', // Équivalent à purple-500
+            },
+        },
     },
     plugins: [],
 }
